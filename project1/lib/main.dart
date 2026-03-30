83671f4abdc4119e7dfb90da42a9e44d812f8c56
Akhilesh Chaudhary <akhileshchy9910@gmail.com>
2026-03-30 14:38:43 +0530

    ..

--- project1/lib/main.dart
+++ project1/lib/main.dart
@@ -22,19 +22,25 @@ class HomePage extends StatelessWidget {
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: [
-              const Color.fromARGB(255, 255, 151, 255),
-              const Color.fromARGB(255, 255, 219, 255),
-            ],
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-          ),
+  colors: [
+    Color.fromARGB(165, 230, 154, 202),
+    Color.fromARGB(171, 255, 221, 225),
+  ],
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+),
         ),
         child: SafeArea(
           child: SingleChildScrollView(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
-              children: [SizedBox(height: 20), Header()],
+              children: [
+                SizedBox(height: 20),
+                Header(),
+                SizedBox(height: 20),
+                Search(),
+                SizedBox(height: 20),
+              ],
             ),
           ),
         ),
@@ -55,6 +61,7 @@ Widget Header() {
             Row(
               children: [
                 Container(
+                  padding: EdgeInsets.all(4),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(100),
                     color: Colors.white,
@@ -77,45 +84,72 @@ Widget Header() {
             ),
           ],
         ),
-        Container(padding: EdgeInsets.all(2),
+        Container(
+          padding: EdgeInsets.all(2),
           decoration: BoxDecoration(
             shape: BoxShape.rectangle,
             borderRadius: BorderRadius.circular(100),
-            color: const Color.fromARGB(213, 255, 255, 255)
+            color: const Color.fromARGB(213, 255, 255, 255),
           ),
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-            Container(padding: EdgeInsets.all(4),
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(96, 213, 208, 208),
-                shape: BoxShape.circle,
-                border:Border.all(color: Colors.white) ,
+              Container(
+                padding: EdgeInsets.all(4),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(96, 213, 208, 208),
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.white),
+                ),
+                child: Badge(
+                  isLabelVisible: true,
+                  label: Text('3'),
+                  child: Icon(Icons.shopping_bag_outlined, size: 30),
+                ),
               ),
-              child: Badge(
-                isLabelVisible: true,
-                label: Text('3',),
-                child: Icon(Icons.shopping_bag_outlined,size: 30,),
+              SizedBox(width: 10),
+              Container(
+                padding: EdgeInsets.all(4),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(96, 213, 208, 208),
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.white),
+                ),
+                child: Badge(
+                  isLabelVisible: true,
+                  label: Text('3'),
+                  child: Icon(Icons.notifications_outlined, size: 30),
+                ),
               ),
-          
-            ),
-            SizedBox(width: 10,),
-            Container(padding: EdgeInsets.all(4),
-              decoration: BoxDecoration(
-                color:const Color.fromARGB(96, 213, 208, 208),
-                shape: BoxShape.circle,
-                border:Border.all(color: Colors.white) ,
-              ),
-              child: Badge(
-                isLabelVisible: true,
-                label: Text('3',),
-                child: Icon(Icons.notifications_outlined,size: 30,),
-              ),
-          
-            ),
-          ]),
+            ],
+          ),
         ),
       ],
+    ),
+  );
+}
+
+Widget Search() {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+    child: Container(
+      padding: EdgeInsets.all(4),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: Expanded(
+        child: TextField(
+          controller: TextEditingController(),
+          decoration: InputDecoration(
+            prefixIcon: Icon(Icons.search),
+            hintText: 'Search',
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.all(10),
+          ),
+        ),
+      ),
     ),
   );
 }
