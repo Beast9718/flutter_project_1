b9e30f786e9cd7d27023da2ce80f06435267b5ca
Akhilesh Chaudhary <akhileshchy9910@gmail.com>
2026-04-07 23:27:39 +0530

    proceeded with buttns

--- project1/lib/signup_page.dart
+++ project1/lib/signup_page.dart
@@ -22,74 +22,114 @@ class SignupPage extends StatelessWidget {
           ),
         ),
         child: SafeArea(
-          child:Padding(
+          child: Padding(
             padding: const EdgeInsets.all(20.0),
             child: Column(
               children: [
-                SizedBox(height: 20,),
-                    Logo(),
-                    SizedBox(height: 40,),
-                    Signup(),
+                SizedBox(height: 20),
+                Logo(),
+                SizedBox(height: 40),
+                Signup(),
               ],
             ),
-          )
-          )
+          ),
         ),
+      ),
     );
-}
-}
-
-Widget Logo(){
-return Row(
-children: [
-  Image(image: AssetImage('assets/images/shirts.webp'),height: 30,width: 30,),
-  SizedBox(width: 10,),
-  Text('CompanyName',style: TextStyle(
-    fontSize: 28,
-    fontWeight: FontWeight.w500,
-  ),)
-],
-);
+  }
 }
 
-Widget Signup(){
-  return Align(
-    alignment: AlignmentGeometry.topLeft,
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'Sign Up Account',
-          style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.w700,
-          ),
+Widget Logo() {
+  return Row(
+    children: [
+      Image(
+        image: AssetImage('assets/images/shirts.webp'),
+        height: 30,
+        width: 30,
+      ),
+      SizedBox(width: 10),
+      Text(
+        'CompanyName',
+        style: TextStyle(fontSize: 28, fontWeight: FontWeight.w500),
+      ),
+    ],
+  );
+}
+
+Widget Signup() {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        'Sign Up Account',
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700),
+      ),
+      SizedBox(height: 10),
+      Text(
+        'Enter your personal data to create\nyour account.',
+        style: TextStyle(
+          fontSize: 16,
+          color: const Color.fromARGB(255, 94, 104, 109),
         ),
-        SizedBox(height: 10,),
-        Text(
-          'Enter your personal data to create\nyour account.',style: TextStyle(
-            fontSize: 16,
-            color: const Color.fromARGB(255, 94, 104, 109),
+      ),
+      SizedBox(height: 10),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadiusGeometry.circular(12),
+            child: OutlinedButton.icon(
+              onPressed: () {},
+              clipBehavior: Clip.antiAlias,
+              label: Text('Google'),
+              icon: Icon(Icons.abc),
+              style: OutlinedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusGeometry.circular(12),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 30,vertical: 10),
+                iconSize: 30,
+                textStyle: TextStyle(fontSize: 18),
+                foregroundColor: Colors.black,
+                side: BorderSide(
+                  color: const Color.fromARGB(255, 125, 124, 124),
+                ),
+              ),
+            ),
           ),
-        ),
-        SizedBox(height: 10,),
-        Expanded(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-                Container(
-                  
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(40),
-                    shape: BoxShape.rectangle,
-                    
-                  ),
-                )
-            ],
+          ClipRRect(
+            borderRadius: BorderRadiusGeometry.circular(12),
+            child: OutlinedButton.icon(
+              onPressed: () {},
+              clipBehavior: Clip.antiAlias,
+              label: Text('Apple'),
+              icon: Icon(Icons.apple),
+              style: OutlinedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusGeometry.circular(12),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 30,vertical: 10),
+                iconSize: 30,
+                textStyle: TextStyle(fontSize: 18),
+                foregroundColor: Colors.black,
+                side: BorderSide(
+                  color: const Color.fromARGB(255, 125, 124, 124),
+                ),
+              ),
+            ),
           ),
-        )
-      ],
-    ),
+        ],
+      ),
+      SizedBox(height: 20),
+      Row(
+        children: [
+          Expanded(child: Divider(thickness: 2,color: Color.fromARGB(255, 125, 124, 124),)),
+          SizedBox(width: 5),
+          Text('OR'),
+          SizedBox(width: 5),
+          Expanded(child: Divider(thickness: 2,color: Color.fromARGB(255, 125, 124, 124),)),
+        ],
+      ),
+    ],
   );
 }
